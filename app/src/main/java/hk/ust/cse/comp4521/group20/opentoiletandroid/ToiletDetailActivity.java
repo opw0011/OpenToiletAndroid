f1dd192747536590303d754dbbd1bf19db1b934d
Benny O <opw0011@gmail.com>
2017-05-01 18:16:13 +0800

    Update toilet detail view with collapsed toolbar

--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/ToiletDetailActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/ToiletDetailActivity.java
@@ -29,14 +29,14 @@ public class ToiletDetailActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(bundle.getString("ToiletName"));
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
