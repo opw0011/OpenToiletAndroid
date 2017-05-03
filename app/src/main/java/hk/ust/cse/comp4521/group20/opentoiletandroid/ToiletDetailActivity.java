3e3ddca648073b6fcde46001e268ef7795d43114
samchan <hschanak@ust.hk>
2017-05-03 21:56:18 +0800

    Fixed a compile error caused by merge

--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/ToiletDetailActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/ToiletDetailActivity.java
@@ -101,17 +101,6 @@ public class ToiletDetailActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.reviewList);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
     }
 
     public void onClick(View v) {
