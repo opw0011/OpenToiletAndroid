f04bb73b7fbd000a56a30071781ce670f25b30ce
Danny Yu <wlyuaa@connect.ust.hk>
2017-05-03 14:19:26 +0800

    Implement login by email

--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package hk.ust.cse.comp4521.group20.opentoiletandroid;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
@@ -15,12 +17,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,10 +46,21 @@ public class MainActivity extends AppCompatActivity
     private Toolbar toolbar;
     private NavigationView navigationView;
 
+    private ImageView profilePic;
+    private TextView profileEmail;
+    private TextView profileName;
+
+    private FirebaseAuth auth;
+
+    public static final int LOGIN = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize FirebaseAuth
+        auth = FirebaseAuth.getInstance();
 
         // Set the initial fragment
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -60,6 +88,12 @@ public class MainActivity extends AppCompatActivity
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // Get references to header
+        View header = navigationView.getHeaderView(0);
+        profilePic = (ImageView) header.findViewById(R.id.profile_pic);
+        profileEmail = (TextView) header.findViewById(R.id.profile_email);
+        profileName = (TextView) header.findViewById(R.id.profile_name);
+
         // TODO:  remove the following Firebase test code
         // Testing write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -83,6 +117,14 @@ public class MainActivity extends AppCompatActivity
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+
+        // Check whether the user is logged in
+        FirebaseUser user = auth.getCurrentUser();
+        if(user != null) {
+            // if yes, set the menu and the header
+            navigationView.getMenu().findItem(R.id.nav_account).setTitle(R.string.nav_account_logout);
+            setHeader(user.getDisplayName(), user.getEmail(), user.getPhotoUrl());
+        }
     }
 
     @Override
@@ -136,7 +178,25 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_alarm) {
 
         } else if (id == R.id.nav_account) {
+            if (auth.getCurrentUser() != null) {
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // user is now signed out
+                                navigationView.getMenu().findItem(R.id.nav_account).setTitle(R.string.nav_account_login);
 
+                                // reset the header
+                                setHeader(null, null, null);
+                            }
+                        });
+        } else {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .build(),
+                        0);
+            }
         } else if (id == R.id.nav_setting) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
@@ -145,5 +205,59 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Retrieve results from log in screen
+        if (requestCode == LOGIN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                navigationView.getMenu().findItem(R.id.nav_account).setTitle(R.string.nav_account_logout);
+
+                // Update the user name and email
+                FirebaseUser user = auth.getCurrentUser();
+                navigationView.getMenu().findItem(R.id.nav_account).setTitle(R.string.nav_account_logout);
+
+                setHeader(user.getDisplayName(), user.getEmail(), user.getPhotoUrl());
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                     return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Log.d(TAG, "No network");
+//                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Log.d(TAG, "Unknown Error");
+//                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+        }
+
+    }
+
+    protected void setHeader(String username, String email, Uri imageURL){
+        if (username != null) {
+            profileName.setText(username);
+            if (email != null) {
+                profileEmail.setText(email);
+            }
+            if (imageURL != null)
+                profilePic.setImageURI(imageURL);
+        } else {
+            profileName.setText("");
+            profileEmail.setText(getText(R.string.not_logged_in));
+        }
     }
 }
