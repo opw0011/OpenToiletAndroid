ffb5eedd788d80b196d7aae992e722cb6f57d6d7
Benny O <opw0011@gmail.com>
2017-05-06 15:58:46 +0800

    Fix #9

--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/WriteToiletReviewActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/WriteToiletReviewActivity.java
@@ -1,10 +1,12 @@
 package hk.ust.cse.comp4521.group20.opentoiletandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,15 +22,15 @@ import java.util.Date;
 import hk.ust.cse.comp4521.group20.opentoiletandroid.data.Review;
 
 public class WriteToiletReviewActivity extends AppCompatActivity {
-    RatingBar ratingBar;
-    SeekBar seekBar;
-    EditText titleText;
-    EditText contentText;
-    Button button;
+    private RatingBar ratingBar;
+    private SeekBar seekBar;
+    private EditText titleText;
+    private EditText contentText;
+    private Button button;
+    private String toiletId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String toiletId ="";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_toilet_review);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -57,19 +59,28 @@ public class WriteToiletReviewActivity extends AppCompatActivity {
                 String formattedDate = sdf.format(new Date());
 
                 DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("review_items/"+ finalToiletId);
+                // TODO: push user id, update the total_score and count
                 mRef.push().setValue(new Review("123", titleText.getText().toString(), contentText.getText().toString(), formattedDate, ratingBar.getRating(), seekBar.getProgress(), ""));
-                //TODO:find a better solution
                 onBackPressed();
             }
         });
+    }
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(WriteToiletReviewActivity.this, ToiletDetailActivity.class);
+        intent.putExtra("ToiletId", toiletId);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
