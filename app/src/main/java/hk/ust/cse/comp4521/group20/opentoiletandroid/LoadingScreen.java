f2436600c43a1788822a3dc62eb674738153263c
Tim <fbfbfbtom@gmail.com>
2017-05-08 15:45:47 +0800

    Add loading screen for Firebase data fetching

---
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/LoadingScreen.java
@@ -0,0 +1,19 @@
+package hk.ust.cse.comp4521.group20.opentoiletandroid;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoadingScreen extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.loading, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/ToiletListFragment.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/ToiletListFragment.java
@@ -1,8 +1,8 @@
 package hk.ust.cse.comp4521.group20.opentoiletandroid;
 
 
-import android.icu.text.DecimalFormat;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,12 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import hk.ust.cse.comp4521.group20.opentoiletandroid.data.Toilet;
-
-import static java.lang.Math.round;
 
 
 /**
@@ -53,7 +54,34 @@ public class ToiletListFragment extends Fragment {
         mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        // show loading screen
+        LoadingScreen loadingScreen = new LoadingScreen();
+        getActivity().getFragmentManager().beginTransaction()
+                .add(R.id.drawer_layout, loadingScreen).commit();
+
+        // get data from Firebase
         DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("toilet_items");
+
+        // remove loading screen when data is ready
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    getActivity().getFragmentManager().beginTransaction()
+                            .remove(loadingScreen).commit();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Snackbar.make(container, "Database error", 3000)
+                        .setAction("OK", v -> getActivity().getFragmentManager().beginTransaction()
+                                .remove(loadingScreen).commit())
+                        .show();
+            }
+        });
+
+        // display data to the user
         mAdapter = new FirebaseRecyclerAdapter<Toilet, ToiletViewHolder>(Toilet.class, R.layout.toilet_list_item, ToiletViewHolder.class, mRef) {
             @Override
             protected void populateViewHolder(ToiletViewHolder toiletViewHolder, Toilet toilet, int position) {
@@ -61,14 +89,13 @@ public class ToiletListFragment extends Fragment {
                 String liftString = toilet.getLift().toString();
                 toiletViewHolder.setToilet(toilet);
                 toiletViewHolder.setToiletId(getRef(position).getKey());
-                toiletViewHolder.setText(String.format("lift: %s rating: %.1f", liftString.substring(1, liftString.length()-1), (double) toilet.getTotal_score() / toilet.getCount()));
+                toiletViewHolder.setText(String.format("lift: %s rating: %.1f", liftString.substring(1, liftString.length() - 1), (double) toilet.getTotal_score() / toilet.getCount()));
             }
         };
         mRecyclerView.setAdapter(mAdapter);
 
         return view;
     }
-
 
 
 }
