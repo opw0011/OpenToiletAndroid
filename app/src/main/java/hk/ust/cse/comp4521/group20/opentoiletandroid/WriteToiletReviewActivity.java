c0ca975f53418f80dfddc0a4fb641726b69cd8e9
Danny Yu <wlyuaa@connect.ust.hk>
2017-05-11 02:54:04 +0800

    Upload photo to Firebase

--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/WriteToiletReviewActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/WriteToiletReviewActivity.java
@@ -1,23 +1,30 @@
 package hk.ust.cse.comp4521.group20.opentoiletandroid;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import hk.ust.cse.comp4521.group20.opentoiletandroid.data.Review;
 
@@ -28,6 +35,18 @@ public class WriteToiletReviewActivity extends AppCompatActivity {
     private EditText contentText;
     private Button button;
     private String toiletId;
+    private Button uploadImageButton;
+
+    private StorageReference storageReference;
+    private FirebaseAuth auth;
+
+    private Uri selectedImageURI;
+
+
+    private static final String TAG = "WriteToiletReview";
+
+
+    protected static final int SELECT_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +54,10 @@ public class WriteToiletReviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_write_toilet_review);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Set up Firebase services
+        storageReference = FirebaseStorage.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
 
         // Enable back to home navigation
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -52,6 +75,8 @@ public class WriteToiletReviewActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button2);
         final String finalToiletId = toiletId;
         button.setOnClickListener(new View.OnClickListener() {
+
+            //TODO: Refactor the entire block
             @Override
             public void onClick(View v) {
                 //TODO: add the image URL
@@ -59,10 +84,38 @@ public class WriteToiletReviewActivity extends AppCompatActivity {
                 String formattedDate = sdf.format(new Date());
 
                 DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("review_items/"+ finalToiletId);
-                // TODO: push user id, update the total_score and count
-                mRef.push().setValue(new Review("123", titleText.getText().toString(), contentText.getText().toString(), formattedDate, ratingBar.getRating(), seekBar.getProgress(), ""));
-                onBackPressed();
+
+                // if there photo attached, upload it
+                if(selectedImageURI != null) {
+                    ContentResolver cR = getApplicationContext().getContentResolver();
+                    MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+                    StorageReference imgRef = storageReference.child("image_items/"+ finalToiletId + "/" + UUID.randomUUID()+ "." + mime.getExtensionFromMimeType(cR.getType(selectedImageURI)));
+                    imgRef.putFile(selectedImageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        @SuppressWarnings("VisibleForTests")
+                        // Suppressed due to bugs with FirebaseStorage
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mRef.push().setValue(new Review(auth.getCurrentUser().getUid(), titleText.getText().toString(), contentText.getText().toString(), formattedDate, ratingBar.getRating(), seekBar.getProgress(), taskSnapshot.getDownloadUrl().toString()));
+                            onBackPressed();
+                        }
+                    });
+                } else {
+                    mRef.push().setValue(new Review(auth.getCurrentUser().getUid(), titleText.getText().toString(), contentText.getText().toString(), formattedDate, ratingBar.getRating(), seekBar.getProgress(), ""));
+
+                    onBackPressed();
+                }
+
             }
+        });
+
+        uploadImageButton = (Button) findViewById(R.id.btn_upload_image);
+        uploadImageButton.setOnClickListener((View v) -> {
+            Intent selectImageIntent = new Intent(Intent.ACTION_PICK);
+            selectImageIntent.setType("image/*");
+            selectImageIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+
+            startActivityForResult(selectImageIntent, SELECT_IMAGE);
         });
     }
 
@@ -82,5 +135,16 @@ public class WriteToiletReviewActivity extends AppCompatActivity {
         intent.putExtra("ToiletId", toiletId);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_IMAGE) {
+            if(resultCode == RESULT_OK) {
+                selectedImageURI = data.getData();
+            }
+        }
     }
 }
