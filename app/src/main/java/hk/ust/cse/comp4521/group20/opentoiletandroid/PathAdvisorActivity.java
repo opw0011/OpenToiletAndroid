40c87bc006f32e788f1993b9dc1dbd9abc96efc9
Danny Yu <wlyuaa@connect.ust.hk>
2017-05-24 18:49:41 +0800

    Add names in Java files

--- app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/PathAdvisorActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/group20/opentoiletandroid/PathAdvisorActivity.java
@@ -1,3 +1,9 @@
+/*
+   #    COMP 4521
+   #    CHAN HON SUM    20192524    hschanak@connect.ust.hk
+   #    O PUI WAI       20198827    pwo@connect.ust.hk
+   #    YU WANG LEUNG   20202032    wlyuaa@connect.ust.hk
+ */
 package hk.ust.cse.comp4521.group20.opentoiletandroid;
 
 import android.content.Intent;
